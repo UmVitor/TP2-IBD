115262db0b33e2648e1cb09730c363e2362f7746
Vitor Barreto <vittor_barreto@hotmail.com>
2022-07-10 18:53:03 -0300

    Adicao de orgao Superior

--- tabelas.sql
+++ tabelas.sql
@@ -73,3 +73,25 @@ CREATE TABLE item_comprado (
                 PRIMARY KEY (id_item),
                 FOREIGN KEY (id_contratacao, id_contrato) REFERENCES contratacao (id_contratacao, id_contrato)
 );
+
+insert into orgao_superior(id_orgao_superior, nome) values (26000,'Ministério da Educação');
+insert into orgao_superior(id_orgao_superior, nome) values (40000,'Ministério do Trabalho');
+insert into orgao_superior(id_orgao_superior, nome) values (52000,'Ministério da Defesa');
+insert into orgao_superior(id_orgao_superior, nome) values (44000,'Ministério do Meio Ambiente');
+insert into orgao_superior(id_orgao_superior, nome) values (41000,'Ministério das Comunicações');
+insert into orgao_superior(id_orgao_superior, nome) values (30000,'Ministério da Justiça e Segurança Pública');
+insert into orgao_superior(id_orgao_superior, nome) values (36000,'Ministério da Saúde');
+insert into orgao_superior(id_orgao_superior, nome) values (22000,'Ministério da Agricultura, Pecuária e Abastec');
+insert into orgao_superior(id_orgao_superior, nome) values (55000,'Ministério da Cidadania');
+insert into orgao_superior(id_orgao_superior, nome) values (54000,'Ministério do Turismo');
+insert into orgao_superior(id_orgao_superior, nome) values (25000,'Ministério da Economia');
+insert into orgao_superior(id_orgao_superior, nome) values (20000,'Presidência da República');
+insert into orgao_superior(id_orgao_superior, nome) values (24000,'Ministério da Ciência, Tecnologia, Inovações');
+insert into orgao_superior(id_orgao_superior, nome) values (39000,'Ministério da Infraestrutura');
+insert into orgao_superior(id_orgao_superior, nome) values (53000,'Ministério do Desenvolvimento Regional');
+insert into orgao_superior(id_orgao_superior, nome) values (63000,'Advocacia-Geral da União');
+insert into orgao_superior(id_orgao_superior, nome) values (32000,'Ministério de Minas e Energia');
+insert into orgao_superior(id_orgao_superior, nome) values (25201,'Banco Central do Brasil - Orçamento Fiscal e ');
+insert into orgao_superior(id_orgao_superior, nome) values (81000,'Ministério da Mulher, Família e Direitos Huma');
+insert into orgao_superior(id_orgao_superior, nome) values (35000,'Ministério das Relações Exteriores');
+insert into orgao_superior(id_orgao_superior, nome) values (37000,'Controladoria-Geral da União');
